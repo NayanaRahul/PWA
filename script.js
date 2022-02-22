c7c296cc8eaae91840d207a0efe4c1e8dda8269c
Nayana Rahul <75112611+NayanaRahul@users.noreply.github.com>
2022-02-22 12:39:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register("/sw.js");
-    }
+    
     /* PWA Install Prompt */
     let deferredPrompt;
     const acceptBtn = document.querySelector('.acceptBtn');
